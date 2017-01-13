d4648cb927db035257016935058fd79996de6db0
ACA1Woodruff <AlanWoodruff53@gmail.com>
2017-01-12 21:14:15 -0600

    Merge master into ACA1Woodruff004
    Conflicts:
    	ACA2016/src/edu/uca/aca2016/stereo/ACA1Woodruff/MyStereo.java
    	ACA2016/src/edu/uca/aca2016/stereo/cpfiles/MyStereo.java

--- src/java/arwcrm/objects/Customer.java
+++ src/java/arwcrm/objects/Customer.java
@@ -13,11 +13,37 @@ package arwcrm.objects;
 import java.io.Serializable;
 
 public class Customer implements Serializable {
+    
     private int id;
-    private String name;
-    private String address;
-    private String contactFirstName;
+    private String customerName;
     private String contactLastName;
+    private String contactFirstName;
+    private int phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String city;
+    private String state;
+    private int postalCode;
+    private String country;
+    private String salesRepEmployeeNumber;
+    private int creditLimit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
     public String getContactLastName() {
         return contactLastName;
@@ -35,34 +61,92 @@ public class Customer implements Serializable {
         this.contactFirstName = contactFirstName;
     }
 
-    public String getAddress() {
-        return address;
+    public int getPhone() {
+        return phone;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    
-    public void setId(int i) {
-        id = i;
+    public void setPhone(int phone) {
+        this.phone = phone;
     }
 
-    public int getId() {
-        return id;
+    public String getAddressLine1() {
+        return addressLine1;
     }
-    
-    public String getName() {
-        return name;
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
     }
-    
-    public void setName(String name) {
-        this.name = name;
+
+    public String getAddressLine2() {
+        return addressLine2;
     }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getAddressLine3() {
+        return addressLine3;
+    }
+
+    public void setAddressLine3(String addressLine3) {
+        this.addressLine3 = addressLine3;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSalesRepEmployeeNumber() {
+        return salesRepEmployeeNumber;
+    }
+
+    public void setSalesRepEmployeeNumber(String salesRepEmployeeNumber) {
+        this.salesRepEmployeeNumber = salesRepEmployeeNumber;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(int creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
     
     public String toString() {
         StringBuffer buffer = new StringBuffer();
         buffer.append("ID: " + this.id + ";");
-        buffer.append("Name: " + name);
+        buffer.append("ContactFirstName: " + contactFirstName);
+        buffer.append("ContactLastName: " + contactLastName);
         return buffer.toString();
     }
 }
